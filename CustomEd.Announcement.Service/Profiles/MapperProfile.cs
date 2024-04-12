e3b86ea71333dd247181da3970808e1022d10e81
= <amanuel.tewodros@a2sv.org>
2024-04-12 10:59:07 +0300

    fix classroom validation and mapping

--- CustomEd.Announcement.Service/Profiles/MapperProfile.cs
+++ CustomEd.Announcement.Service/Profiles/MapperProfile.cs
@@ -2,16 +2,22 @@ using AutoMapper;
 using CustomEd.Announcement.Service.DTOs;
 using CustomEd.Announcement.Service.Model;
 using CustomEd.Contracts.Classroom.Events;
+using CustomEd.Shared.Data.Interfaces;
 
 namespace CustomEd.Announcement.Service.Profiles
 {
     public class MapperProfile : Profile
     {
-        public MapperProfile()
+        private readonly IGenericRepository<ClassRoom> _classroomRepository;
+        public MapperProfile(IGenericRepository<ClassRoom> classroomRepository)
         {
+            _classroomRepository = classroomRepository;
+
             CreateMap<Model.Announcement, AnnouncementDto>().ReverseMap();
-            CreateMap<Model.Announcement, CreateAnnouncementDto>().ReverseMap();
-            CreateMap<Model.Announcement, UpdateAnnouncementDto>().ReverseMap();
+            CreateMap<CreateAnnouncementDto, Model.Announcement>()
+                .ForMember(dest => dest.ClassRoom, opt => opt.MapFrom(src => _classroomRepository.GetAsync(src.ClassRoomId)));
+            CreateMap<UpdateAnnouncementDto, Model.Announcement>()
+                .ForMember(dest => dest.ClassRoom, opt => opt.MapFrom(src => _classroomRepository.GetAsync(src.ClassRoomId)));
 
             CreateMap<ClassRoom, ClassroomCreatedEvent>().ReverseMap();
             CreateMap<ClassRoom, ClassroomUpdatedEvent>().ReverseMap();
