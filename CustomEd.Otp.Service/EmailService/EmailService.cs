7ebe9bead6fed5191a8334b73c2a8de8ab11a589
= <amanuel.tewodros@a2sv.org>
2024-04-14 23:38:49 +0300

    Implement Email Service 2

--- CustomEd.Otp.Service/EmailService/EmailService.cs
+++ CustomEd.Otp.Service/EmailService/EmailService.cs
@@ -1,23 +1,25 @@
 using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Options;
 
 namespace CustomEd.Otp.Service.Email.Service;
 
 public class EmailService : IEmailService
 {
     private readonly SmtpClient _smtpClient;
+    private readonly IOptions<EmailSettings> _emailSettings;
 
-    public EmailService()
+    public EmailService(IOptions<EmailSettings> emailSettings)
     {
+        _emailSettings = emailSettings;
         _smtpClient = new SmtpClient
         {
-            Host = "smtp.example.com", 
-            Port = 587, 
+            Host = _emailSettings.Value.MailServer, 
+            Port = _emailSettings.Value.MailPort, 
             EnableSsl = true,
-            Credentials = new NetworkCredential("username@example.com", "password") 
+            Credentials = new NetworkCredential(_emailSettings.Value.Username, _emailSettings.Value.Password) 
         };
     }
-
     public async Task SendEmailAsync(string email, string subject, string message)
     {
         var mailMessage = new MailMessage
--- CustomEd.Otp.Service/Program.cs
+++ CustomEd.Otp.Service/Program.cs
@@ -1,7 +1,10 @@
+
+using CustomEd.Otp.Service.Email.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
