ab721e58b29162a821d4a9bab5217f3bc72e339f
= <amanuel.tewodros@a2sv.org>
2024-04-18 23:31:15 +0300

    Produce UserEvent when Verified

--- CustomEd.User.Service/src/Consumers/UserVerifiedEventConsumer.cs
+++ CustomEd.User.Service/src/Consumers/UserVerifiedEventConsumer.cs
@@ -2,18 +2,25 @@ using MassTransit;
 using CustomEd.Shared.Data.Interfaces;
 using CustomEd.Shared.Model;
 using CustomEd.Contracts.Otp;
+using AutoMapper;
+using CustomEd.User.Student.Events;
+using CustomEd.User.Teacher.Events;
 
 namespace CustomEd.User.Service.Consumers
 {
-    public class StudentCreatedEventConsumer : IConsumer<UserVerifiedEvent>
+    public class UserVerifiedEventConsumer : IConsumer<UserVerifiedEvent>
     {
         private readonly IGenericRepository<Model.Student> _studentRepository;
         private readonly IGenericRepository<Model.Teacher> _teacherRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMapper _mapper;
 
-        public StudentCreatedEventConsumer(IGenericRepository<Model.Student> studentRepository, IGenericRepository<Model.Teacher> teacherRepository)
+        public UserVerifiedEventConsumer(IGenericRepository<Model.Student> studentRepository, IGenericRepository<Model.Teacher> teacherRepository, IPublishEndpoint publishEndpoint, IMapper mapper)
         {
             _studentRepository = studentRepository;
             _teacherRepository = teacherRepository;
+            _publishEndpoint = publishEndpoint;
+            _mapper = mapper;
         }
 
         public async Task Consume(ConsumeContext<UserVerifiedEvent> context)
@@ -25,12 +32,18 @@ namespace CustomEd.User.Service.Consumers
                 var student = await _studentRepository.GetAsync(userVerifiedEvent.UserId);
                 student.IsVerified = true;
                 await _studentRepository.UpdateAsync(student);
+                var studentCreatedEvent = _mapper.Map<StudentCreatedEvent>(student);
+                await _publishEndpoint.Publish(studentCreatedEvent);
+             
+                
             }
             else if(userVerifiedEvent.Role == Role.Teacher)
             {
                 var teacher = await _teacherRepository.GetAsync(userVerifiedEvent.UserId);
                 teacher.IsVerified = true;
                 await _teacherRepository.UpdateAsync(teacher);
+                var teacherCreatedEvent = _mapper.Map<TeacherCreatedEvent>(teacher);
+                await _publishEndpoint.Publish(teacherCreatedEvent);
             }
         }
     }
--- CustomEd.User.Service/src/Controllers/StudentController.cs
+++ CustomEd.User.Service/src/Controllers/StudentController.cs
@@ -1,18 +1,18 @@
-using Microsoft.AspNetCore.Mvc;
-using CustomEd.User.Service.DTOs;
 using AutoMapper;
-using CustomEd.User.Service.Validators;
-using CustomEd.User.Service.PasswordService.Interfaces;
-using CustomEd.Shared.JWT.Interfaces;
-using Microsoft.AspNetCore.Authorization;
-using CustomEd.Shared.Data.Interfaces;
-using CustomEd.Shared.Response;
-using MassTransit;
-using CustomEd.Shared.JWT.Contracts;
 using CusotmEd.User.Servce.DTOs;
-using CustomEd.User.Student.Events;
+using CustomEd.Shared.Data.Interfaces;
 using CustomEd.Shared.JWT;
+using CustomEd.Shared.JWT.Contracts;
+using CustomEd.Shared.JWT.Interfaces;
+using CustomEd.Shared.Response;
+using CustomEd.User.Service.DTOs;
 using CustomEd.User.Service.Model;
+using CustomEd.User.Service.PasswordService.Interfaces;
+using CustomEd.User.Service.Validators;
+using CustomEd.User.Student.Events;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CustomEd.User.Service.Controllers
 {
@@ -20,40 +20,72 @@ namespace CustomEd.User.Service.Controllers
     [Route("api/user/student")]
     public class StudentController : UserController<Model.Student>
     {
-
-        public StudentController(IGenericRepository<Model.Student> userRepository, IMapper mapper, IPasswordHasher passwordHasher, IJwtService jwtService, IPublishEndpoint publishEndpoint, IHttpContextAccessor httpContextAccessor) : base(userRepository, mapper, passwordHasher, jwtService, publishEndpoint, httpContextAccessor)
-        {
-        }
-
+        public StudentController(
+            IGenericRepository<Model.Student> userRepository,
+            IMapper mapper,
+            IPasswordHasher passwordHasher,
+            IJwtService jwtService,
+            IPublishEndpoint publishEndpoint,
+            IHttpContextAccessor httpContextAccessor
+        )
+            : base(
+                userRepository,
+                mapper,
+                passwordHasher,
+                jwtService,
+                publishEndpoint,
+                httpContextAccessor
+            ) { }
 
         [HttpGet("student-id")]
-        public async Task<ActionResult<SharedResponse<StudentDto>>> SearchStudentBySchoolId([FromQuery] string id)
+        public async Task<ActionResult<SharedResponse<StudentDto>>> SearchStudentBySchoolId(
+            [FromQuery] string id
+        )
         {
-            var student = await _userRepository.GetAsync(u => u.StudentId == id && u.IsVerified == true) ;
+            var student = await _userRepository.GetAsync(u =>
+                u.StudentId == id && u.IsVerified == true
+            );
             var studentDto = _mapper.Map<StudentDto>(student);
-            return Ok(SharedResponse<StudentDto>.Success(studentDto, "Students retrieved successfully"));
+            return Ok(
+                SharedResponse<StudentDto>.Success(studentDto, "Students retrieved successfully")
+            );
         }
 
         [HttpGet("student-name")]
-        public async Task<ActionResult<SharedResponse<IEnumerable<StudentDto>>>> SearchStudentByName([FromQuery] string name)
+        public async Task<
+            ActionResult<SharedResponse<IEnumerable<StudentDto>>>
+        > SearchStudentByName([FromQuery] string name)
         {
-            var students = await _userRepository.GetAllAsync(u => u.IsVerified && (u.FirstName!.Contains(name) || u.LastName!.Contains(name)));
+            var students = await _userRepository.GetAllAsync(u =>
+                u.IsVerified && (u.FirstName!.Contains(name) || u.LastName!.Contains(name))
+            );
             var studentsDto = _mapper.Map<IEnumerable<StudentDto>>(students);
-            return Ok(SharedResponse<IEnumerable<StudentDto>>.Success(studentsDto, "Students retrieved successfully"));
+            return Ok(
+                SharedResponse<IEnumerable<StudentDto>>.Success(
+                    studentsDto,
+                    "Students retrieved successfully"
+                )
+            );
         }
 
         [HttpPost]
-        public async Task<ActionResult<SharedResponse<Model.Student>>> CreateUser([FromBody] CreateStudentDto studentDto)
+        public async Task<ActionResult<SharedResponse<Model.Student>>> CreateUser(
+            [FromBody] CreateStudentDto studentDto
+        )
         {
             var createStudentDtoValidator = new CreateStudentDtoValidator(_userRepository);
             var validationResult = await createStudentDtoValidator.ValidateAsync(studentDto);
             if (!validationResult.IsValid)
             {
-                return BadRequest(SharedResponse<Model.Student>.Fail("Invalid input", validationResult.Errors.Select(e => e.ErrorMessage).ToList()));
+                return BadRequest(
+                    SharedResponse<Model.Student>.Fail(
+                        "Invalid input",
+                        validationResult.Errors.Select(e => e.ErrorMessage).ToList()
+                    )
+                );
             }
             var passwordHash = _passwordHasher.HashPassword(studentDto.Password);
             studentDto.Password = passwordHash;
-
 
             var student = _mapper.Map<Model.Student>(studentDto);
             student.Role = Model.Role.Student;
@@ -61,11 +93,13 @@ namespace CustomEd.User.Service.Controllers
             await _userRepository.CreateAsync(student);
 
             var studentCreatedEvent = _mapper.Map<StudentCreatedEvent>(student);
-            await _publishEndpoint.Publish(studentCreatedEvent);
-            return CreatedAtAction(nameof(GetUserById), new { id = student.Id }, SharedResponse<Model.Student>.Success(student, "User created successfully"));
-
+            // await _publishEndpoint.Publish(studentCreatedEvent);
+            return CreatedAtAction(
+                nameof(GetUserById),
+                new { id = student.Id },
+                SharedResponse<Model.Student>.Success(student, "User created successfully")
+            );
         }
-
 
         [Authorize]
         [HttpDelete("{id}")]
@@ -73,41 +107,66 @@ namespace CustomEd.User.Service.Controllers
         {
             if (id == Guid.Empty || await _userRepository.GetAsync(id) == null)
             {
-                return BadRequest(SharedResponse<StudentDto>.Fail("Invalid Id", new List<string> { "Invalid id" }));
+                return BadRequest(
+                    SharedResponse<StudentDto>.Fail("Invalid Id", new List<string> { "Invalid id" })
+                );
             }
 
-            var identityProvider = new IdentityProvider((IHttpContextAccessor)HttpContext, _jwtService);
+            var identityProvider = new IdentityProvider(
+                (IHttpContextAccessor)HttpContext,
+                _jwtService
+            );
             var currentUserId = identityProvider.GetUserId();
 
-            if(currentUserId != id)
+            if (currentUserId != id)
             {
-                return Unauthorized(SharedResponse<StudentDto>.Fail("Unauthorized", new List<string> { "Unauthorized" }));
+                return Unauthorized(
+                    SharedResponse<StudentDto>.Fail(
+                        "Unauthorized",
+                        new List<string> { "Unauthorized" }
+                    )
+                );
             }
 
-
+            var student = await _userRepository.GetAsync(id);
             await _userRepository.RemoveAsync(id);
-            var studentDeletedEvent = new StudentDeletedEvent{Id = id};
-            await _publishEndpoint.Publish(studentDeletedEvent);
+            var studentDeletedEvent = new StudentDeletedEvent { Id = id };
+            if (student.IsVerified == true)
+            {
+                await _publishEndpoint.Publish(studentDeletedEvent);
+            }
             return Ok(SharedResponse<StudentDto>.Success(null, "User deleted successfully"));
         }
 
         [Authorize]
         [HttpPut]
-        public async Task<ActionResult<SharedResponse<StudentDto>>> UpdateUser([FromBody] UpdateStudentDto studentDto)
+        public async Task<ActionResult<SharedResponse<StudentDto>>> UpdateUser(
+            [FromBody] UpdateStudentDto studentDto
+        )
         {
             var updateStudentDtoValidator = new UpdateStudentDtoValidator(_userRepository);
             var validationResult = await updateStudentDtoValidator.ValidateAsync(studentDto);
             if (!validationResult.IsValid)
             {
-                return BadRequest(SharedResponse<StudentDto>.Fail("Invalid input", validationResult.Errors.Select(e => e.ErrorMessage).ToList()));
+                return BadRequest(
+                    SharedResponse<StudentDto>.Fail(
+                        "Invalid input",
+                        validationResult.Errors.Select(e => e.ErrorMessage).ToList()
+                    )
+                );
             }
 
             var identityProvider = new IdentityProvider(_httpContextAccessor, _jwtService);
             var currentUserId = identityProvider.GetUserId();
 
-            if(currentUserId != studentDto.Id)
+            if (currentUserId != studentDto.Id)
             {
-                return Unauthorized(SharedResponse<StudentDto>.Fail("Unauthorized", new List<string> { "Unauthorized" }));
+                return Unauthorized(
+                    SharedResponse<StudentDto>.Fail(
+                        "Unauthorized",
+                        new List<string> { "Unauthorized" }
+                    )
+                );
             }
 
             var passwordHash = _passwordHasher.HashPassword(studentDto.Password);
@@ -118,19 +177,19 @@ namespace CustomEd.User.Service.Controllers
 
             await _userRepository.UpdateAsync(student);
             var studentUpdatedEvent = _mapper.Map<StudentCreatedEvent>(student);
-            await _publishEndpoint.Publish(studentUpdatedEvent);
-            
+            if (student.IsVerified == true)
+            {
+                await _publishEndpoint.Publish(studentUpdatedEvent);
+            }
             return Ok(SharedResponse<StudentDto>.Success(null, "User updated successfully"));
-
         }
 
         [HttpPost("login")]
-        public override async Task<ActionResult<SharedResponse<UserDto>>> SignIn([FromBody] LoginRequestDto request)
+        public override async Task<ActionResult<SharedResponse<UserDto>>> SignIn(
+            [FromBody] LoginRequestDto request
+        )
         {
-
             return await base.SignIn(request);
-
         }
-
     }
 }
