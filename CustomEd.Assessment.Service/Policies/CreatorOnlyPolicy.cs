0572acd2d04b8a0b59d7f8247580552f1b41e178
= <amanuel.tewodros@a2sv.org>
2024-06-18 21:30:30 +0300

    add admin privilage

--- CustomEd.Assessment.Service/Policies/CreatorOnlyPolicy.cs
+++ CustomEd.Assessment.Service/Policies/CreatorOnlyPolicy.cs
@@ -2,6 +2,7 @@ using CustomEd.Assessment.Service.Model;
 using CustomEd.Shared.Data.Interfaces;
 using CustomEd.Shared.JWT;
 using CustomEd.Shared.JWT.Interfaces;
+using CustomEd.Shared.Model;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -27,9 +28,10 @@ public class CreatorOnlyPolicy : AuthorizationHandler<CreatorOnlyRequirement>
         var classroomId = Guid.Parse((string)_httpContextAccessor.HttpContext!.Request.RouteValues["classRoomId"]!);
         var identityProvider = new IdentityProvider(_httpContextAccessor, _jwtService);
         var userId = identityProvider.GetUserId();
+        var userRole = identityProvider.GetUserRole();
 
         var classroom = await _classRoomRepository.GetAsync(classroomId);
-        if (classroom.CreatorId == userId)
+        if (classroom.CreatorId == userId || userRole == Role.Admin)
         {
             context.Succeed(requirement);
         }
